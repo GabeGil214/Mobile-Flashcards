f15ccba0367d99b048bd82ed675700b41007805b
Gabe <gilgabe214@gmail.com>
2019-01-10 22:18:04 -0500

    fix: wrap root navigator in appcontainer

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import AppContainer from './components/AppContainer'
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import Deck from './components/Deck'
+import NewDeck from './components/NewDeck'
+
+const Stack = createStackNavigator({
+  Deck: {
+    screen: Deck,
+  },
+  NewDeck: {
+    screen: NewDeck,
+  }
+})
+
+const AppContainer = createAppContainer(Stack)
 
 export default class App extends React.Component {
   render() {
